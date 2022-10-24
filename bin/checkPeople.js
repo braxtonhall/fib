7cdf8b8dfca8e8513fdbc1ed67a701420d8527cb
braxtonhall <braxtonjhall@gmail.com>
2022-10-24 12:18:35 -0700

    Add a PR workflow

---
+++ bin/checkPeople.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+
+const PEOPLE_PATH = "./people.json";
+
+const [actor] = process.argv.slice(2);
+
+console.log(`Triggered by ${actor}`);
+
+if (!fs.existsSync(PEOPLE_PATH)) {
+    throw "No people file found";
+}
+
+let people;
+try {
+    people = JSON.parse(fs.readFileSync(PEOPLE_PATH));
+} catch (err) {
+    throw "Could not read people.json as JSON";
+}
+
+const person = people.find(person => person.github === actor);
+if (!person) {
+    throw `${actor} isn't in people.json!`;
+}
+
+if (!(person.entries && person.entries.length > 0)) {
+    throw `${actor} is missing entries!`;
+}
+
+console.log("Checked!");
