1a7717e25e93599666803bb11f80c87117b01114
s-lando <slandolando@hotmail.com>
2022-11-13 15:01:02 -0800

    elixir fibs

---
+++ entries/s-lando/fib.ex
@@ -0,0 +1,19 @@
+defmodule Fib do
+  # elegant
+  def fib(n) when n < 2, do: n
+  def fib(n), do: fib(n - 1) + fib(n - 2)
+
+  
+  # less elegant
+  def fib2(n) do
+    fib2(n, 0, 1)
+  end
+
+  def fib2(n, current, next) do
+    cond do
+      n == 0 -> current
+      n == 1 -> next
+      true -> fib2(n - 1, next, current + next)
+    end
+  end
+end
